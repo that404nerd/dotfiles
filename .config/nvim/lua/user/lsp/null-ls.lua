1d98b97cd98519bae686cef976f525321d0d7cb5
systemcoding <revanthnagmunagala@gmail.com>
2022-03-06 15:30:03 +0530

    Prettier formatting

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -11,10 +11,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		-- diagnostics.flake8
+		diagnostics.flake8
 	},
 })
 
