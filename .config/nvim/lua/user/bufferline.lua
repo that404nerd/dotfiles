2362101b48b5f7931f1a369e51bf13e69cac32c4
System Coding <revanthnagmunagala@gmail.com>
2023-05-22 13:58:38 +0530

    Updated neovim configs

--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -21,7 +21,6 @@ require('bufferline').setup {
     color_icons = true,
     show_buffer_icons = true,
     show_buffer_close_icons = true,
-    show_buffer_default_icon = true,
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true,
