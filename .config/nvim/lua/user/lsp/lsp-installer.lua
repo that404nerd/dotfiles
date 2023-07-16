e6f8fb015585aa72336877b73cdabf20be66b3e6
systemcoding <revanthnagmunagala@gmail.com>
2023-07-16 12:03:12 +0530

    Replace old nvim configs with nvchad configs

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-lsp-installer").setup {}
-local lspconfig = require("lspconfig")
-
-lspconfig.lua_ls.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.ccls.setup {}
--- .config/nvim/lua/user/lualine.lua
+++
@@ -1,3 +0,0 @@
-require('lualine').setup {
-  options = { theme  = 'ayu_mirage',section_separators = '', component_separators = '' },
-}
