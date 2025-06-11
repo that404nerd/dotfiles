32d14b176c18d5a48ca21fdee7b1e711e1885b2a
that404nerd <revanthnagmunagala@gmail.com>
2025-06-11 14:31:32 +0530

    update

---
+++ .config/nvim/lua/plugins/godot-cpp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = { "clangd", "--background-index", "--clang-tidy" },
+        },
+      },
+    },
+  },
+}
+
